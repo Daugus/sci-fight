d516a429817f494ba16fbfba77615efabb6bee38
lukaquindon08 <lukacarmona115@gmail.com>
2023-02-27 10:31:53 +0100

    barras de vida funcionales flata pasar datos correctamente

---
+++ components/HealthBar.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { PropType } from 'vue';
+import Character from '~~/utils/types';
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    character: { required: true, type: Object as PropType<Character> },
+    currentHealth: { required: true, type: Number },
+  },
+  computed: {
+    charactersCurrentHealth() {
+      return (this.currentHealth / this.character.health) * 100;
+    },
+  },
+};
+// character.health
+</script>
+
+<template>
+  <div class="flex h-6 w-full overflow-visible bg-red-600">
+    <div
+      :class="['h-full', 'transition-all']"
+      :style="`width: ${charactersCurrentHealth}%; background-color: ${character.color}`"
+    ></div>
+  </div>
+</template>
+
+<style lang="scss"></style>
--- pages/arena.vue
+++ pages/arena.vue
@@ -3,18 +3,40 @@ export default {
   data() {
     return {
       stage: '',
+      currentCharacter: astraeus,
+      otro: galvus,
+      p1Health: 0,
+      p2Health: 0,
     };
   },
   mounted() {
     // Generar un escenario aleatorio
     let random = Math.floor(Math.random() * (4 - 1) + 1);
     this.stage = `url(/src/img/stages/${random}.gif)`;
+    this.p1Health = this.currentCharacter.health;
+    this.p2Health = this.otro.health;
   },
+  methods: {},
 };
 </script>
 
 <template>
-  <div class="stage"></div>
+  <div class="stage">
+    <div class="absolute top-8 flex w-full items-center justify-center space-x-5 bg-slate-500 px-11 text-center align-middle">
+      <HealthBar
+        :character="currentCharacter"
+        :currentHealth="p1Health"
+      ></HealthBar>
+      <img
+        class="h-9"
+        src="\src\img\assets\stats_health.png "
+      />
+      <HealthBar
+        :currentHealth="p2Health"
+        :character="otro"
+      ></HealthBar>
+    </div>
+  </div>
   <Character
     :player-number="1"
     :controls="{ attack: 'w', left: 'a', right: 'd' }"
