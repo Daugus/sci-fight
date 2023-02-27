3b2b3af6e7ad6a8d3458d1cde6a5e5f69c10a694
GleZz23 <ikerglez110@gmail.com>
2023-02-27 12:39:01 +0100

    implementacion de suelo en arena

--- pages/arena.vue
+++ pages/arena.vue
@@ -3,18 +3,22 @@ export default {
   data() {
     return {
       stage: '',
+      floor: '',
     };
   },
   mounted() {
     // Generar un escenario aleatorio
     let random = Math.floor(Math.random() * (4 - 1) + 1);
     this.stage = `url(/src/img/stages/${random}.gif)`;
+    this.floor = `url(/src/img/stages/${random}.png)`;
   },
 };
 </script>
 
 <template>
-  <div class="stage"></div>
+  <div class="stage">
+    <div class="floor"></div>
+  </div>
   <Character
     :player-number="1"
     :controls="{ attack: 'w', left: 'a', right: 'd' }"
@@ -34,6 +38,16 @@ export default {
   background-image: v-bind(stage);
   background-size: cover;
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: bottom;
+
+  .floor {
+    width: 100vw;
+    height: 10vh;
+    position: absolute;
+    bottom: 0;
+
+    background-image: v-bind(floor);
+    background-size: 20%;
+  }
 }
 </style>
