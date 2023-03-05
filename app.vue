57cdae931e534802083115cf491296d5a220d3ee
GleZz23 <ikerglez110@gmail.com>
2023-03-05 18:50:59 +0100

    enlaces de navegacion quitados

--- app.vue
+++ app.vue
@@ -1,11 +1,4 @@
 <template>
-  <header class="absolute top-1 z-50 text-2xl">
-    <ul class="mx-auto flex w-screen items-center justify-center gap-5">
-      <li><NuxtLink to="/">Menú</NuxtLink></li>
-      <li><NuxtLink to="/arena">Arena</NuxtLink></li>
-    </ul>
-  </header>
-
   <div class="flex h-screen items-center justify-center text-2xl">
     <NuxtPage />
   </div>
